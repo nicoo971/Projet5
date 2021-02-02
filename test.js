5f1d3d8a18efaf201a96cd456c1da79fb5a887ca
nicoo971 <nicolaspouget@outlook.fr>
2021-02-02 14:37:45 +0100

    testp5

---
+++ test.js
@@ -0,0 +1,33 @@
+class Episode {
+    constructor(title, duration, hasBeenWatched) {
+        this.title = title;
+        this.duration = duration;
+        this.hasBeenWatched = hasBeenWatched;
+    }
+}
+
+function TestEpisode() {
+    let firstEpisode = new Episode("RotS", 120, true);
+    console.log(firstEpisode);
+
+}
+
+function TestScopeText() {
+    let text = "test2";
+    let age = 40;
+    if (age > 20) {
+        let text2 = "test3";
+        console.log('t2', text2); //test3
+        text = "test4";
+    }
+
+    //alert(text2); //undefined
+    console.log('t', text); // test2 ou test4 en fonction de l'age 
+    return "fini";
+}
+
+TestEpisode();
+let resultat = TestScopeText();
+console.log('result', resultat)
+
+//alert("coucou")
